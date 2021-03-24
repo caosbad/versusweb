ddd6c970a10f57b8cf75dc9530b02a3e9959f4d6
rubenpa <rubenpa321@gmail.com>
2021-03-24 15:13:58 -0700

    fcl

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as fcl from "@onflow/fcl";
 
+import "../config";
 import Nav from "../components/general/Nav";
 import Footer from "../components/general/Footer";
 
 const Main = ({ children }) => {
+  useEffect(() => {
+    // console.log(fcl);
+  }, []);
   return (
     <div id="main">
       <Nav />
