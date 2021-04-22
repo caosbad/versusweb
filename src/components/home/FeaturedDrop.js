f443f2ea951bc6bee163a7292010ced78766f7f4
rubenpa <rubenpa321@gmail.com>
2021-04-22 11:53:12 -0700

    banner

--- src/components/home/FeaturedDrop.js
+++ src/components/home/FeaturedDrop.js
@@ -5,19 +5,20 @@ const FeaturedDrop = () => {
   return (
     <div
       className="bg-cover featured-image w-full relative z-20 bg-center"
-      style={{ backgroundImage: `url(${withPrefix("/images/kamp.png")})` }}
+      style={{ backgroundImage: `url(${withPrefix("/images/bull.png")})` }}
     >
-      <div className="container py-16 h-full flex flex-col justify-between text-white">
+      <div className="absolute left-0 top-0 w-full h-full bg-black-500 backdrop-filter bg-opacity-30" />
+      <div className="container py-16 h-full flex flex-col justify-between text-white z-10 relative">
         <div>
           <h6 className="font-lato font-semibold text-xl">Introducing.</h6>
           <h2 className="text-8xl font-ibm mt-1 tracking-tighter relative -left-1">
-            Kamp
+            Kinger
           </h2>
         </div>
         <div className="flex flex-col sm:flex-row justify-between items-center">
           <span className="font-lato text-xl uppercase mb-4 sm:mb-0">
-            <span className="font-light">Dropping </span>
-            <span className="font-bold">April</span>
+            <span className="font-light">Testing </span>
+            <span className="font-bold">Now</span>
           </span>
           <Link
             to="/artist/drops/1"
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const Home = () => {
       <Helmet>
         <title>Home</title>
       </Helmet>
+      <FeaturedDrop />
       <Landing />
       <WhatsDifferent />
       <NotRewarded />
