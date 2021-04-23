3cb0fdb90f8e5da887c4fa661c6ab8807a426625
rubenpa <rubenpa321@gmail.com>
2021-04-23 14:37:07 -0700

    basic seo

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -6,6 +6,7 @@ import "../config";
 import Nav from "../components/general/Nav";
 import Footer from "../components/general/Footer";
 import icon from "../images/icon.png";
+import { withPrefix } from "gatsby";
 
 const Main = ({ children }) => {
   const [user, setUser] = useState({ loggedIn: null });
@@ -13,8 +14,37 @@ const Main = ({ children }) => {
   return (
     <div id="main">
       <Helmet titleTemplate="%s | Versus" defaultTitle="Versus">
-        <meta name="description" content="Better for art" />
         <link rel="icon" type="image/png" href={icon} sizes="16x16" />
+        <title>Versus - Better for Art</title>
+        <meta name="title" content="Versus - Better for Art" />
+        <meta
+          name="description"
+          content="Versus is a novel NFT art marketplace that works to empower the artist and the collector."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://versus-flow.art/" />
+        <meta property="og:title" content="Versus - Better for Art" />
+        <meta
+          property="og:description"
+          content="Versus is a novel NFT art marketplace that works to empower the artist and the collector."
+        />
+        <meta
+          property="og:image"
+          content={withPrefix("/images/versussocial.png")}
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="http://versus-flow.art/" />
+        <meta property="twitter:title" content="Versus - Better for Art" />
+        <meta
+          property="twitter:description"
+          content="Versus is a novel NFT art marketplace that works to empower the artist and the collector."
+        />
+        <meta
+          property="twitter:image"
+          content={withPrefix("/images/versussocial.png")}
+        />
       </Helmet>
       <Nav user={user} />
       {children(user)}
